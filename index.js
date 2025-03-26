beb186f7c2c98b0bc1fc771ee651a4e349b6b796
lengocsangsang <lesang88@gmail.com>
2025-03-27 01:43:23 +0700

    adding menu button for hompage

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ import newsRender from "./Controller/newsRender.js";
 import investmentFunction from "./Controller/investment.js";
 
 // HOMEPAGE
+function disableScroll() {
+  document.documentElement.style.overflow = "hidden"; // Disable scrolling
+  document.body.style.overflow = "hidden";
+  document.body.style.height = "100vh"; // Prevent body from exceeding viewport
+  document.body.style.position = "fixed"; // Fix body position to prevent movement
+}
+disableScroll();
 // TITLE UPDATING
 updateClock();
 
