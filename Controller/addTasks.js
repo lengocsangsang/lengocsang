4845dc80cfcb86360986d357986452c8e9d73476
lengocsangsang <lesang88@gmail.com>
2025-03-28 01:24:53 +0700

    adding delete button

--- Controller/addTasks.js
+++ Controller/addTasks.js
@@ -9,26 +9,43 @@ export default function addTaskToMainSection() {
     taskInputBox.addEventListener("keydown", function (e) {
       if (e.key === "Enter") {
         e.preventDefault();
-
-        const taskContent = taskInputBox.value.trim();
+        let dataInlocalStorage =
+          JSON.parse(localStorage.getItem("tasks")) || [];
+        const index =
+          dataInlocalStorage.length === 0 ? 0 : dataInlocalStorage.length;
+        let taskContent = taskInputBox.value.trim();
         if (taskContent === "") {
           return alert("You haven't input your task yet");
         }
-        addTaskToDom(taskContent);
+        addTaskToDom(taskContent, index);
         saveTaskToLocalStorage(taskContent);
         taskInputBox.value = "";
       }
     });
 
+    taskList.addEventListener("click", function (e) {
+      const deleteBtn = e.target.closest(".task-delete-button");
+      let dataInlocalStorage = JSON.parse(localStorage.getItem("tasks")) || [];
+      if (deleteBtn) {
+        dataInlocalStorage.splice(deleteBtn.dataset.key, 1);
+        deleteBtn.parentElement.remove();
+        localStorage.setItem("tasks", JSON.stringify(dataInlocalStorage));
+      }
+    });
+
     clearTaskBtn.addEventListener("click", removeItemFromLocalStorage);
 
-    function addTaskToDom(taskContent) {
+    function addTaskToDom(taskContent, index) {
       const taskItem = document.createElement("li");
       const deleteBtn = document.createElement("span");
+
       taskItem.classList.add("task-item");
       taskItem.textContent = taskContent;
-      deleteBtn.textContent = "-";
+
+      deleteBtn.textContent = " -";
       deleteBtn.classList.add("task-delete-button");
+      deleteBtn.dataset.key = index;
+
       taskItem.appendChild(deleteBtn);
       taskList.appendChild(taskItem);
     }
@@ -53,8 +70,8 @@ export default function addTaskToMainSection() {
 
     function loadPage() {
       let tasks = getTasksFromLocalStorage();
-      tasks.forEach((task) => {
-        addTaskToDom(task);
+      tasks.forEach((task, index) => {
+        addTaskToDom(task, index);
       });
     }
 
